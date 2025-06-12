d6af02c4bedb25cf637272bce62fe9866553c7cc
f0xdl <deluca.fernaut@gmail.com>
2025-06-12 12:00:36 +0200

    add configs add .env.example add yaml configuration

---
+++ internal/configs/config.go
@@ -0,0 +1,26 @@
+package configs
+
+import (
+	"errors"
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	TelegramToken string `env:"TELEGRAM_TOKEN"`
+	ChatID        int64  `env:"CHAT_ID"`
+	MqttServer    string `env:"MQTT_SERVER"`
+	MqttTopic     string `env:"MQTT_TOPIC"`
+	BotDb         string `env:"BOT_DB"`
+}
+
+func LoadConfig() (Config, error) {
+	cfg := &Config{}
+	if err := godotenv.Load(); err != nil {
+		return Config{}, errors.New("error loading .env file: " + err.Error())
+	}
+	if err := env.Parse(cfg); err != nil {
+		return Config{}, errors.New("error parsing .env file: " + err.Error())
+	}
+	return *cfg, nil
+}
