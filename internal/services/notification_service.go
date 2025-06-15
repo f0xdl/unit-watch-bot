4df05c53df8ce6514b129460eb0e9f6ad681053c
f0xdl <deluca.fernaut@gmail.com>
2025-06-15 11:48:28 +0200

    fix args processed in notification_service.go

--- internal/services/notification_service.go
+++ internal/services/notification_service.go
@@ -46,12 +46,13 @@ func (ns *Notification) prepareMessages(templateName string, chatIds map[int64]s
 	result := map[int64]string{}
 	var err error
 	for chatId, lang := range chatIds {
+		msgArgs := maps.Clone(args)
 		if !templates.LangCodeIsValid(lang) {
 			log.Warn().Int64("chat", chatId).Str("lang", lang).Msg("invalid language code")
 			lang = string(templates.English)
 		}
-		prepareArgs(templates.LangCode(lang), args)
-		result[chatId], err = ns.templater.Render(templateName, args)
+		prepareArgs(templates.LangCode(lang), msgArgs)
+		result[chatId], err = ns.templater.Render(templateName, msgArgs)
 		if err != nil {
 			log.Error().Err(err).Int64("chatId", chatId).Msg("render error")
 		}
@@ -60,8 +61,12 @@ func (ns *Notification) prepareMessages(templateName string, chatIds map[int64]s
 }
 
 func prepareArgs(lang templates.LangCode, args map[string]interface{}) {
-	args["ChangedAt"] = templates.FormatChangedAt(args["ChangedAt"].(time.Time), lang)
-	args["LastSeen"] = templates.FormatSeenAgo(args["LastSeen"].(time.Time), lang)
+	if v, ok := args["ChangedAt"]; ok {
+		args["ChangedAt"] = templates.FormatChangedAt(v.(time.Time), lang)
+	}
+	if v, ok := args["LastSeen"]; ok {
+		args["LastSeen"] = templates.FormatSeenAgo(v.(time.Time), lang)
+	}
 
 	for k, v := range args {
 		if str, ok := v.(string); ok {
